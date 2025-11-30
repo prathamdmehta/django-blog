e7317cc66937584f09e294eddedc34c100fe59d0
Pratham Mehta <mehta2935@gmail.com>
2025-11-30 19:15:03 +0000

    sorting blogs

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogs'
+    'django_browser_reload',
+    'blogs',
 ]
 
 MIDDLEWARE = [
