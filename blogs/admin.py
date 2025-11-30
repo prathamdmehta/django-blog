07b9546279960d49af6f7eb010281a197d1775b2
Pratham Mehta <mehta2935@gmail.com>
2025-11-30 14:34:01 +0000

    blog model

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Blog
+
+# (Pre-Populated Slug:) this is supposed to be done whenever you create a slug, now slug means that for example https:chrome/blogs/this-is-sports-news...so after blog/ whatever text is written is supposed to be the slug for what blog-post you are viewing, in admin page using this function ifincase the title of blog is very large than we can't time the slug manually so this function basically automatically fills up the slug on bassis of what title the user is giving to create the blog
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'status')
+    list_editable = ('is_featured', ) 
+    # if you want to make any data-model in admin to be editable
+
+# we wrote cateogry__category_name because we wnat to search with the name of the category because as we have mentioned in the models.py in Category function than we wnat to display category_name so thats y we wrote that...
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -1,10 +1,38 @@
 from django.db import models
-import datetime
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length = 50, unique = True)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Meta:
-    verbose_name_plural = 'categories'
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
+
+# in the search field in admin panel when we search for Draft or Published than it will search for that section, earlier we wrote 0 and 1 which is not the correct way, because we user searches on basis of status than if he search with draft or published key-word than nothing will be displayed because we colud have used 0 and 1 in our code...best practice is to write whatever you want the user will be searching in correct way for status
+
+STATUS_CHOICES = (
+    # (0, "Draft"),
+    ("Draft", "Draft"),
+    # (1, "Published")
+    ("Published", "Published")
+)
+class Blog(models.Model):
+    title = models.CharField(max_length = 100)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #using cascade because when you delete specific category the blog post associated should also be deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # so when user is deleted hte blog post of that user should also be deleted
+    featured_image = models.ImageField(upload_to='uploads/%d/%m/%Y')
+    short_description= models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=2000)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Draft")
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
