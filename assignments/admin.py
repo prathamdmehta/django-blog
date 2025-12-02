766f64751dd16b9be995765536c540459dd85803
Pratham Mehta <mehta2935@gmail.com>
2025-12-02 00:38:49 +0000

    blogs and search features

---
+++ assignments/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import About
+
+class AboutAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        count = About.objects.all().count()
+        if count == 0:
+            return True
+        return False
+
+# Register your models here.
+admin.site.register(About, AboutAdmin)
--- blog_main/settings.py
+++ blog_main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_browser_reload',
     'blogs',
+    'assignments'
 ]
 
 MIDDLEWARE = [
